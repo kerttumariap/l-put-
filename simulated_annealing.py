fba3cf3315856642d46d15931872247b4084d20d
kerttumariap <kerttumariapeensoo@gmail.com>
2018-06-14 19:52:30 +0300

    Update simulated_annealing.py

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -122,48 +122,54 @@ def incr_i(data,n):
 def inte(lst):
     return [int(l) for l in lst]
 
+
+#----------------------------------------------------------------------------------------------------------------------
+
+# Uuritav kiud 
 D_630HP = incr_i(D_koef_f('630HP_disp.xls'),1000)
 
+# Valitud klaasitüübid
 D_FS=D_koef(quartz_B1,quartz_B2,quartz_B3,quartz_C1,quartz_C2,quartz_C3)#---------------1
 D_BK7=D_koef(BK7_B1,BK7_B2,BK7_B3,BK7_C1,BK7_C2,BK7_C3)#--------------------------------2
 D_sap=D_koef(sapphire_B1,sapphire_B2,sapphire_B3,sapphire_C1,sapphire_C2,sapphire_C3)#--3
 D_CaF2=D_koef(CaF2_B1,CaF2_B2,CaF2_B3,CaF2_C1,CaF2_C2,CaF2_C3)#-------------------------4
 D_MgF2=D_koef(MgF2_B1,MgF2_B2,MgF2_B3,MgF2_C1,MgF2_C2,MgF2_C3)#-------------------------5
 D_ZnSe=D_koef(ZnSe_B1,ZnSe_B2,ZnSe_B3,ZnSe_C1,ZnSe_C2,ZnSe_C3)#-------------------------6
-D_air=D_koef_air()#---------------------------------------------------------------------7
+
+klaasid=['Kvartsklaas', 'BK7', 'Safiir', 'Kaltsiumfluoriid', 'Magneesiumfluoriid', 'Tsinkseleniid']
 
 
 # Vaadeldav lainepikkuste vahemik ühe nanomeetrise sammuga
 wl=np.linspace(600,770,170)
 Di=D_630HP[1][0:len(wl)]
+
+# Kiu pikkus millimeetrites
+li=419 # 419 mm 
 # Teisendame dispersioonikoefitsendi ühikutes ühiku kilomeeter millimeetriteks, sest vaatleme kiudu, mis on 419mm
 Di=np.divide(Di,1e6)
-li=419
 
+# Valitud klaaside dispersioonikoefitsentide väärtused valitud lainepikkuste vahemikus wl
 D=[D_FS[1][0:len(wl)], D_BK7[1][0:len(wl)], D_sap[1][0:len(wl)], D_CaF2[1][0:len(wl)], D_MgF2[1][0:len(wl)],D_ZnSe[1][0:len(wl)]]
-#D=[D_FS[1][0:len(wl)]]
 D=np.asarray(D)
+# Teisendame dispersioonikoefitsendi ühikutes ühiku kilomeeter millimeetriteks
 D=np.divide(D,1e6)
-#state=[419]
+
+# Valime algväärtuse, kus kvartsklaasi pikkus on 424 mm ning teiste valitud klaaside pikkused on 0 mm
 state=[424, 0, 0, 0, 0, 0]
-#state=[419, 0, 0, 0, 0]
 state=np.asarray(state)
 
-
+# Minimeeritav funktsioon
 def energy(state):
     y1=np.power(np.multiply(np.multiply(Di,li),1e2),1)
-    #y2=np.power(np.multiply(np.multiply(D[0],state[0])+np.multiply(D[1],state[1])+np.multiply(D[2],state[2])+np.multiply(D[3],state[3])+np.multiply(D[4],state[4]),1e2),1)
     y2=np.power(np.multiply(np.multiply(D[0],state[0])+np.multiply(D[1],state[1])+np.multiply(D[2],state[2])+np.multiply(D[3],state[3])+np.multiply(D[4],state[4])+np.multiply(D[5],state[5]),1e2),1)
-    #y2=np.power(np.multiply(np.multiply(D[0],state[0]),1e2),1)
     eq=y1-y2
-    #eq = np.power(np.multiply(Di,li),1)-np.power(np.multiply(D[0],state[0]),1)-np.power(np.multiply(D[1],state[1]),1)-np.power(np.multiply(D[2],state[2]),1)-np.power(np.multiply(D[3],state[3]),1)-np.power(np.multiply(D[4],state[4]),1)
     eq=np.multiply(np.sum(np.abs(eq)),1)
     return eq
 
 
 print(energy(state))
 
-
+# Valime ühe klaasi suurima võimaliku pikkuse (700 mm) ja muudame käesolevat väärtust vastavalt sellele, n-ö kontrollitult juhuslikult
 def move(state):
     i = random.randint(0,5)
     if state[i]<700 and state[i]>0: 
@@ -179,10 +185,14 @@ annealer = Annealer(energy, move)
 schedule = annealer.auto(state, minutes=5)
 state, e = annealer.anneal(state, schedule['tmax'], schedule['tmin'], schedule['steps'], updates=1000)
 state, e = annealer.anneal(state, 10000, 0.1, 100000, updates=30)
-#print('esimene')
-#print(state)
-#state, e = annealer.auto(state, 4)
-print(state)  # the "final" solution
+
+# Parim klaaside kombinatsioon
+for i in range(len(state)): 
+    print(klaasid[i])
+    print(state[i])
+
+# Minimeeritava funktsiooni väärtus, mis vastab parimale klaasi kombinatsioonile
+print('Minimeeritava funktsiooni väärtus:')
 print(energy(state))
 
 
